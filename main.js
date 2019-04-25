c37c1fb3204281784fb279e67bb0af5466587186
Ryunosuke Hirota <a.y.k_24@i.softbank.jp>
2019-04-25 16:57:50 +0900

    second commit

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 $(window).on('load', function() {
-  $("#fade img").addClass("fadein");
+  $(".hero img").fadeIn(2000);
+  $(".scroll, .scroll span").fadeIn(4000);
+
+  /* toppage-bottom */
+  var TopBtn = $('#PageTopBtn');
+  TopBtn.hide();
+  // スクロール位置が100でボタンを表示
+  $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+          TopBtn.fadeIn();
+      }
+      else {
+          TopBtn.fadeOut();
+      }
+  });
+  // ボタンを押下するとトップへ移動
+  TopBtn.click(function() {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 300);
+      return false;
+  });
+
   });
