1c9df37a1c87fad3628e4b93ba80085afe3c079a
Ryunosuke Hirota <a.y.k_24@i.softbank.jp>
2019-04-26 13:57:50 +0900

    third commit

---
+++ index.php
@@ -0,0 +1,248 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-138733030-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-138733030-1');
+</script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="岐阜県の塗装、外壁塗装、屋根塗装、建築塗装、塗替えのことなら美濃塗装工業へ">
+    <title>有限会社 美濃塗装工業 | 岐阜県美濃市の塗装屋</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Sawarabi+Mincho" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Sawarabi+Mincho" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Russo+One" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <script src="./main.js" charset="utf-8"></script>
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <?php wp_head(); ?>
+  </head>
+
+  <body>
+    <div class="header home-header">
+      <nav>
+        <ul class="hide-p">
+          <li class="logo"><a href="https://minopaintindustry.com/"><img src="./images/namelogo.png"></a></li>
+          <li class="hide"><a href="https://minopaintindustry.com/"><div class="hd-nav"><div>Home</div><div>ホーム</div></div></a></li>
+          <li class="hide"><a href="./about.html"><div class="hd-nav"><div>About</div><div>会社概要</div></div></a></li>
+          <li class="hide"><a href=""><div class="hd-nav"><div>Works<span class="mente">(メンテ中)</span></div><div>施工実績</div></div></a></li>
+          <li class="hide"><a href="./recruit.html"><div class="hd-nav"><div>Recruit</div><div>採用情報</div></div></a></li>
+          <li class="hide"><a href="./form.html"><div class="hd-nav"><div>Form</div><div>お問合せ</div></div></a></li>
+        </ul>
+      </nav>
+      <div class="navToggle">
+        <span></span><span></span><span></span><span>menu</span>
+      </div>
+      <nav class="globalMenuSp">
+        <ul>
+          <li class="logo"><a href="https://minopaintindustry.com/"><img src="./images/namelogo.png"></a></li>
+          <li><a href="https://minopaintindustry.com/"><div><i class="fas fa-home fa-2x pd-right"></i>ホーム</div></a></li>
+          <li><a href="./about.html"><div><i class="fas fa-building fa-2x pd-right"></i>会社概要</div></a><a href="./about.html#philosophy">- 企業理念</a><a href="./about.html#corporate">- 会社概要</a><a href="./about.html#job">- 業務内容</a></li>
+          <li><a href=""><div><i class="fas fa-paint-brush fa-2x pd-right"></i>施工実績<span class="mente">(メンテ中)</span></div></a></li>
+          <li><a href="./recruit.html"><div><i class="fas fa-hand-holding-heart fa-2x pd-right"></i>採用情報</div></a></li>
+          <li><a href="./form.html"><div><i class="far fa-paper-plane fa-2x pd-right"></i>お問合せ</div></a></li>
+        </ul>
+      </nav>
+    </div>
+    <div class="bg">
+      <div class="greeting-container headline">
+        <h2><span>G</span>reeting</h2>
+        <p class="content-header wf-sawarabimincho headline">ご挨拶</p>
+        <div class="greeting">
+            <img class="greeting-image" src="./images/motto.png">
+          <div class="message">
+            <p class="wf-sawarabimincho headline">弊社のWEBサイトをご覧いただき、誠にありがとうございます。<br>
+               弊社は、1990年(平成2年)に塗装業として創業しました。一般の住宅から店舗や工場、学校などの公共工事と幅広く携わらせていただいています。これも、ひとえに多くの方とのご縁のお陰であります。<br>
+               「縁があれば千里」という、ことわざがあります。これは、縁があれば遠く離れた人と夫婦になったりするが、なければ口を聞かないどころか出会うこともない、という意味で使われています。
+               この言葉でも分かるように、人の繋がりには『縁』という、何か特別な力があるように思います。とするならば、
+               その縁を家族、友人、職場と言わず、一人ひとりがもつ関わりをもっと大切にしたいものです。<br>
+               私たちは塗装を通して、いただいた『ご縁』をより良いものにするため、皆様の人生が笑顔と彩りで溢れるよう、日々邁進して参ります。</p>
+          </div>
+        </div>
+      </div>
+  </div>
+
+    <div class="features-container headline">
+      <h2><span>R</span>eason</h2>
+      <p class="content-header headline">選ばれる理由</p>
+      <p class="reason-description headline">綺麗に仕上げることはプロとして当たり前。それ以上の価値を提供したい。</p>
+      <div class="features headline">
+        <div class="feature">
+          <h3><i class="fas fa-bolt fa-2x"></i>迅速な対応</h3>
+          <p class="feature-addition">お困りの際は「今すぐに」でも対応してほしいものです。弊社では、適切かつスピーディーな対応ができる体制を整えております。</p>
+        </div>
+        <div class="feature">
+          <h3><i class="far fa-sticky-note fa-2x"></i>徹底した品質管理</h3>
+             <p class="feature-addition">弊社では、長年の経験から得た技術と知識から、それぞれの建物の状態にあった工法・材料で最適に施工できます。
+             また、塗装は工事終了後では「手を抜いたかどうか」分かりにくくなります。弊社では工程ごとに写真を撮り、まとめて工事終了後にお渡しいたします。</p>
+        </div>
+        <div class="feature">
+          <h3><i class="fas fa-heartbeat fa-2x"></i>安心のアフターフォロー</h3>
+          <p class="feature-addition">工事が終了した時点で終わりではなく、末長いお付き合いをさせていただきたいと考えております。
+             些細な事でも構いませんので、お気付きの点・お困りの点があればお気になさらずお申し付けください。</p>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="criterion-container headline">
+        <h2><span>C</span>riterion</h2>
+        <p class="content-header headline">塗替えの目安</p>
+        <div class="key">
+          <p class="criterion-addition">塗替えの目安はおおよそ10年程度と言われておりますが、立地や気候などの条件により様々です。<br>
+           下記にご紹介する状態を一つの目安として参考にしてみてください。
+          </p>
+        </div>
+      <div class="criterion">
+        <div class="criterion-content">
+          <h3 class="marker_yellow"><i class="fas fa-check fa-2x"></i>チョーキング(白亜化)</h3>
+          <div class="criterion-flexbox headline">
+            <img src="./images/chalking.jpg" alt="チョーキング">
+            <div class="box17">
+              <p class="photo-description">塗装表面が紫外線や雨、風により劣化して塗料の顔料がチョークのように白い粉状になって現れる現象です。</p>
+            </div>
+          </div>
+        </div>
+        <div class="criterion-content">
+          <h3 class="marker_yellow"><i class="fas fa-check fa-2x"></i>クラック(亀裂)</h3>
+          <div class="criterion-flexbox headline">
+            <img src="./images/clack.jpg" alt="クラック">
+            <div class="box17">
+              <p class="photo-description">外壁などに亀裂が入ると、そこから雨水が建物の中に侵入してくる可能性があります。
+               雨漏りや建物自体の劣化の原因にもなります。</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="criterion-content">
+        <h3 class="marker_yellow"><i class="fas fa-check fa-2x"></i>目地(コーキング)の劣化</h3>
+        <div class="criterion-flexbox headline">
+          <img src="./images/caulking.jpg" alt="劣化したコーキング">
+          <div class="box17">
+            <p class="photo-description">外壁や窓のつなぎ目部分にあるコーキングがひび割れていたり、欠損していたりすると水漏れ、建物の劣化の原因となります。</p>
+          </div>
+        </div>
+      </div>
+      <div class="criterion-content">
+        <h3 class="marker_yellow"><i class="fas fa-check fa-2x"></i>錆(サビ)</h3>
+        <div class="criterion-flexbox headline">
+          <img src="./images/rust.jpg" alt="さびた鉄">
+          <div class="box17">
+            <p class="photo-description">鉄部にサビが発生して放っておくと腐食が進んで、ひどい場合には欠損して穴があいてしまいます。</p>
+          </div>
+        </div>
+      </div>
+      <div class="criterion-end">
+        <p><i class="fas fa-paint-roller fa-2x"></i>塗替えの時期の判断は、専門の知識がある各業者の間でも様々ではありますが、<br>
+          調査・見積もりは無料となっておりますので、ぜひ一度お問い合わせください。</p>
+      </div>
+    </div>
+
+
+    <div class="flow-container headline">
+      <h2><span>F</span>low</h2>
+      <p class="content-header">工事の流れ</p>
+      <div class="flow">
+        <div class="flow-content">
+          <img class="step" src="./images/step1.png">
+          <p class="flow-head">お問合せ</p>
+          <img class="human-image" src="./images/tel.png">
+          <p class="flow-description">お電話またはメールよりお問い合わせください。</p>
+          <p class="flow-description">
+            <i class="fas fa-mobile-alt fa-2x"></i>
+            <a href="tel:0575-35-3501">0575-35-3501</a>
+          </p>
+          <p class="flow-description">
+            <i class="far fa-envelope fa-2x"></i>
+            <a href="mailto:minopaint.daiki@gmail.com">minopaint.daiki@gmail.com</a>
+          </p>
+        </div>
+        <div class="flow-content">
+          <img class="step" src="./images/step2.png">
+          <p class="flow-head">現地調査</p>
+          <img class="human-image" src="./images/check.png">
+          <p class="flow-description">お問い合わせいただくと、現地でお話をお伺いさせていただき、建物の状態の確認、施工面積の計測などをさせていただきます。</p>
+        </div>
+        <div class="flow-content">
+          <img class="step" src="./images/step3.png">
+          <p class="flow-head">お見積もり・提案</p>
+          <img class="human-image" src="./images/proposal.png">
+          <p class="flow-description">現地調査やお客様のお声をもとに、様々なパターンの仕様(塗料や塗装の工程、金額など)を適切に選択してご提案させていただき、仕様を決定いたします。<br>
+            <span>※調査・お見積もりは無料です。</span></p>
+        </div>
+        <div class="flow-content">
+          <img class="step" src="./images/step4.png">
+          <p class="flow-head">工事施工</p>
+          <img class="human-image" src="./images/paint.png">
+          <p class="flow-description">お見積もり・仕様に納得していただきましたら、工程など工事の説明をさせていただいた後、着工いたします。</p>
+        </div>
+        <div class="flow-content">
+          <img class="step" src="./images/step5.png">
+          <p class="flow-head">工事終了確認</p>
+          <img class="human-image" src="./images/bow.png">
+          <p class="flow-description">工事終了報告書などの書類をお渡しして、工事に問題がないかご確認していただいた上、工事終了となります。</p>
+        </div>
+        <div class="flow-content">
+          <img class="step" src="./images/step6.png">
+          <p class="flow-head">アフターフォロー</p>
+          <img class="human-image" src="./images/shakehand.png">
+          <p class="flow-description">工事が終了して、塗装以外のお住まいの事、何でも構いません。何かございましたらお気軽にご相談ください。</p>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <div class="footer">
+        <div class="footer-container">
+          <div class="footer-container-left">
+            <P class="live">〜<span class="red">L</span>ive <span class="blue">A</span> <span class="yellow">C</span>olorful <span class="green">L</span>ife〜</p>
+            <p class="name wf-sawarabimincho"><span class="wf-sawarabimincho">有限会社</span>美濃塗装工業</p>
+            <p class="en-name">Mino Paint Industry Ltd.</p>
+          </div>
+          <div class="footer-container-right">
+            <p class="responsive-block">
+              <a href="tel:0575-35-3501">
+              <i class="fas fa-mobile-alt fa-2x"></i>
+              <span class="ft-red headline">0575-35-3501</span></a>
+            </p>
+            <p>(営業時間　9:00 ~ 17:00)</p>
+            <p>
+              <a href="mailto:minopaint.daiki@gmail.com">
+                <i class="far fa-envelope fa-2x"></i>
+                <span class="ft-red headline">minopaint.daiki@gmail.com</span>
+              </a>
+            </p>
+          </div>
+        </div>
+        <div class="header-link">
+          <ul>
+            <li>
+              <a class="ft-nav" href="https://minopaintindustry.com/"><i class="fas fa-chevron-circle-right"></i>Home</a>
+            </li>
+            <li><a class="ft-nav" href="./about.html"><i class="fas fa-chevron-circle-right"></i>About</a></li>
+            <li><a class="ft-nav" href=""><i class="fas fa-chevron-circle-right"></i>Works<span class="mente">(メンテ中)</span></a></li>
+            <li><a class="ft-nav" href="./recruit.html"><i class="fas fa-chevron-circle-right"></i>Recruit</a></li>
+              <li><a class="ft-nav" href="./form.html"><i class="fas fa-chevron-circle-right"></i>Form</a></li>
+          </ul>
+        </div>
+        <p class="copy">&copy; 2018 Minopaint Industry Ltd.</p>
+      </div>
+    </footer>
+    <script>
+      ScrollReveal().reveal('.headline', { easing: 'ease', duration: 2000, distance: '400px', opacity: 1, scale: 1 });
+    </script>
+    <?php wp_footer(); ?>
+  </body>
+
+</html>
--- main.js
+++ main.js
@@ -1,25 +1,81 @@
 $(window).on('load', function() {
-  $(".hero img").fadeIn(2000);
-  $(".scroll, .scroll span").fadeIn(4000);
+    $(".hero img").fadeIn(2000);
+    $(".scroll, .scroll span").fadeIn(4000);
 
-  /* toppage-bottom */
-  var TopBtn = $('#PageTopBtn');
-  TopBtn.hide();
-  // スクロール位置が100でボタンを表示
-  $(window).scroll(function() {
-      if ($(this).scrollTop() > 100) {
-          TopBtn.fadeIn();
-      }
-      else {
-          TopBtn.fadeOut();
-      }
-  });
-  // ボタンを押下するとトップへ移動
-  TopBtn.click(function() {
-      $('body,html').animate({
-          scrollTop: 0
-      }, 300);
-      return false;
+    /* toppage-bottom */
+    var TopBtn = $('#PageTopBtn');
+    TopBtn.hide();
+    // スクロール位置が100でボタンを表示
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            TopBtn.fadeIn();
+        }
+        else {
+            TopBtn.fadeOut();
+        }
+    });
+    // ボタンを押下するとトップへ移動
+    TopBtn.click(function() {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 300);
+        return false;
+    });
   });
 
+  // スライドイン
+  $(window).on('load scroll', function(){
+
+    var elem = $('.boxInner');
+
+    elem.each(function () {
+
+      var isPlay = 'isPlay';
+      var elemOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+
+      if(scrollPos > elemOffset - wh + (wh / 4)){
+        $(this).addClass(isPlay);
+      }
+    });
+  });
+
+  (function ($) {
+    // 画面下からどの程度の位置でエフェクトするかの設定
+    var EffectH = 100;
+    // どのくらい動かすかの設定
+    var EffectTop = 100;
+    // エフェクト時間(ms)
+    var EffectTime = 800;
+    // 事前準備として少し下げたところに透明にして配置。
+    $('.my-effect').css({ position: 'relative', left: 0, top: EffectTop, opacity: 0, transition: EffectTime + 'ms' });
+    // スクロールイベント
+    $(window).on('scroll load', function() {
+      var scTop = $(this).scrollTop();
+      var scBottom = scTop + $(this).height();
+      var effectPos = scBottom - EffectH;
+      // エフェクト対象それぞれの位置とスクロール位置を比べて
+      // 範囲に入っていればエフェクトを実行する
+      $('.my-effect').each( function() {
+        var thisPos = $(this).offset().top;
+        if ( thisPos < effectPos ) {
+          $(this).css({ 'opacity' : 1, top: 0 });
+        }
+      });
+    });
+  })(jQuery);
+
+  jQuery(function(){
+    jQuery(window).scroll(function (){
+        jQuery('.fadein').each(function(){
+            var elemPos = jQuery(this).offset().top;
+            var scroll = jQuery(window).scrollTop();
+            var windowHeight = jQuery(window).height();
+            if (scroll > elemPos - windowHeight + 100){
+                jQuery(this).addClass('scrollin');
+            }
+        });
+    });
+    jQuery(window).scroll();
   });
